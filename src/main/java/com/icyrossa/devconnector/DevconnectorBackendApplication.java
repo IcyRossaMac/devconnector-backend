c40b77dd45b1f2c907efb6d989f7c40ff0c6d7aa
Rossa McLaughlin <icyrossa@gmail.com>
2020-10-22 11:10:45 +0100

    Initial Java backend set up

---
+++ src/main/java/com/icyrossa/devconnector/DevconnectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.icyrossa.devconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevconnectorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevconnectorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icyrossa/devconnector/DevconnectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icyrossa.devconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevconnectorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
